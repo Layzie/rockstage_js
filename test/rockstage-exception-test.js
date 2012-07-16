b17c7353ee3e202e8e2e552e5ff0ace799f2d448
HIRAKI Satoru <saruko313@gmail.com>
2012-07-17 02:05:25 +0900

    add exception test

---
+++ test/rockstage-exception-test.js
@@ -0,0 +1,36 @@
+buster.testCase('RockstageExceptionTest', {
+  setUp: function() {
+    localStorage.clear();
+    sessionStorage.clear();
+  },
+  'RS.put() should be throw error if 1st argument isn\'t Object': function() {
+    assert.exception(function() {
+      RS.put('hoge');
+    });
+    assert.exception(function() {
+      RS.put(['hoge']);
+    });
+    assert.exception(function() {
+      RS.put(7);
+    });
+    assert.exception(function() {
+      RS.put(true);
+    });
+  },
+  'RS.get() should be throw error if 1st argument isn\'t String': function() {
+    RS.put({foo: 'hoge', bar: 'fuga', baz: 'hogera'});
+
+    assert.exception(function() {
+      RS.get(['hoge']);
+    });
+    assert.exception(function() {
+    RS.get({foo: hoge});
+    });
+    assert.exception(function() {
+      RS.get(7);
+    });
+    assert.exception(function() {
+      RS.get(true);
+    });
+  }
+});
